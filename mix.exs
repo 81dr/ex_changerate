c04dca4c88b8a4fbfd3ed979350643d362b2da09
81dr <81dr@tuta.io>
2020-06-03 09:38:12 +0000

    Initalize repository

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule ExChangerate.MixProject do
+  @moduledoc false
+  use Mix.Project
+
+  def project do
+    [
+      app: :ex_changerate,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :httpoison]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 1.6"},
+      {:poison, "~> 4.0"},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+      {:inch_ex, "~> 2.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
